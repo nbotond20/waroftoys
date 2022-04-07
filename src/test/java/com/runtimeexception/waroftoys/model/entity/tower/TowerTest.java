b7b1df82c4d72aedea454c33d49ee4c097c6636d
Szalai Bence <m1mjir@inf.elte.hu>
2022-04-07 09:10:58 +0200

    Pathfinding and Tower test files created.

---
+++ src/test/java/com/runtimeexception/waroftoys/model/entity/tower/TowerTest.java
@@ -0,0 +1,16 @@
+package com.runtimeexception.waroftoys.model.entity.unit;
+
+import static org.junit.Assert.*;
+
+public class TowerTest {
+    @Test
+    public void rangeTest(){
+        
+    }
+    
+    @Test
+    public void abilityTest(){
+
+    }
+    
+}
